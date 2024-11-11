a3ee09161d860a139418e179a688136c9bfefbe0
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2024-11-10 19:07:12 -0500

    Implemented fixed social and mail links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { IconHexagonLetterC } from "@tabler/icons-react";
+import { IconHexagonLetterD } from "@tabler/icons-react";
 import SideBar from "./SideBar";
 
 const links=["About","Work","Experience","Skills","Contact"];
@@ -12,7 +12,7 @@ const Header=()=>{
 return (
     <nav className="flex bg-bgColor h-[18vh] px-10  justify-between items-center">
         
-        <IconHexagonLetterC className="z-10" size={60} color="#64FFDA" stroke={1.25}/>
+        <IconHexagonLetterD className="z-10" size={60} color="#64FFDA" stroke={1.25}/>
         <div className="md:flex gap-8 hidden">
             {navLinks(false)}
         </div>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,8 +1,14 @@
 import Header from "./Header";
+import Mail from "./Mail";
+import Social from "./Social";
 
 const HomePage=()=>{
-    return (
-        <Header/>
+    return ( <>
+        <Header />
+        <Mail />
+        <Social />
+        </>
+        
     )
 };
 export default HomePage;
